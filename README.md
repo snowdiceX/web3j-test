f8cdf8a486e7a8908dff8922dbb74511ebe54c30
snowdiceX <snowdice@outlook.com>
2021-05-04 15:57:22 +0800

    create

---
+++ README.md
@@ -0,0 +1,45 @@
+# web3j test app
+
+## gradle
+
+$ gradle tasks
+
+$ gradle assemble
+
+## web3j-cli
+
+[web3j/web3j-cli](https://github.com/web3j/web3j-cli)
+
+v1.4.1
+
+## web3j
+
+``` plain
+$ web3j wallet create  
+0x81a532c7d746b24dc452a0f08974d8a2a4e94fd9  
+[address info](https://goerli.etherscan.io/address/0x81a532c7d746b24dc452a0f08974d8a2a4e94fd9)
+
+$ cd /opt/gopath/src/github.com/web3j/Web3App/build
+
+$ export WEB3J_WALLET_PATH=./keystore/UTC--2021-04-30T03-35-50.269000000Z--81a532c7d746b24dc452a0f08974d8a2a4e94fd9.json; \
+    export WEB3J_WALLET_PASSWORD=12345678; \
+    export WEB3J_NODE_URL="https://goerli.infura.io/v3/62d0c9419a1748b0bfd37cecda72d694"; \
+    java -jar ./libs/Web3App-0.1.0-all.jar  
+
+[Tx info](https://goerli.etherscan.io/tx/0xbd1d9c53fe38d01a69909e110e6a359f4fb87bd27641e631b85c88a998f2018e)
+
+# import private-key?
+$ web3j wallet import [private-key]
+Error!!!???:
+No TypeConverter registered for java.util.Optional of field java.util.Optional<String> org.web3j.console.wallet.subcommands.WalletImportCommand.privateKey
+
+```
+
+## codes
+
+``` plain
+$ web3j wallet - github.com/web3j/web3j-cli/org.web3j.console.wallet.subcommands.WalletImportCommand
+
+https://ethereum.stackexchange.com/questions/41072/generate-private-key-and-address-using-web3j
+
+```
---
+++ src/main/java/org/web3j/Web3App.java
@@ -0,0 +1,45 @@
+package org.web3j;
+
+import java.math.BigInteger;
+
+import org.web3j.crypto.Credentials;
+import org.web3j.crypto.WalletUtils;
+import org.web3j.generated.contracts.HelloWorld;
+import org.web3j.protocol.Web3j;
+import org.web3j.protocol.http.HttpService;
+import org.web3j.tx.gas.DefaultGasProvider;
+import org.web3j.tx.gas.StaticGasProvider;
+
+/**
+ * <p>This is the generated class for <code>web3j new helloworld</code></p>
+ * <p>It deploys the Hello World contract in src/main/solidity/ and prints its address</p>
+ * <p>For more information on how to run this project, please refer to our <a href="https://docs.web3j.io/quickstart/#deployment">documentation</a></p>
+ */
+public class Web3App {
+
+	private static final String nodeUrl = System.getenv().getOrDefault("WEB3J_NODE_URL", "<node_url>");
+	private static final String walletPassword = System.getenv().getOrDefault("WEB3J_WALLET_PASSWORD", "<wallet_password>");
+	private static final String walletPath = System.getenv().getOrDefault("WEB3J_WALLET_PATH", "<wallet_path>");
+
+    public static final BigInteger GAS_PRICE = BigInteger.valueOf(4_100_000_000L);
+	public static final BigInteger GAS_LIMIT = BigInteger.valueOf(900_000);
+    
+	public static void main(String[] args) throws Exception {
+		System.out.println("System.getenv() ...");
+		System.out.println("nodeUrl: "+nodeUrl);
+		System.out.println("walletPath: "+walletPath);
+		System.out.println("walletPassword: "+walletPassword);
+		
+		Credentials credentials = WalletUtils.loadCredentials(walletPassword, walletPath);
+		Web3j web3j = Web3j.build(new HttpService(nodeUrl));
+		System.out.println("Deploying HelloWorld contract ...");
+//		HelloWorld helloWorld = HelloWorld.deploy(web3j, credentials, new DefaultGasProvider(), "Hello Blockchain World!").send();
+		StaticGasProvider gas = new StaticGasProvider(GAS_PRICE, GAS_LIMIT);
+		System.out.println("new gas ...");
+		HelloWorld helloWorld = HelloWorld.deploy(web3j, credentials, gas,
+				"Hello Blockchain World!").send();
+		System.out.println("Contract address: " + helloWorld.getContractAddress());
+		System.out.println("Greeting method result: " + helloWorld.greeting().send());
+	}
+}
+
